c23cc919a4ec11b2334289c64b6239d160407d48
NastyaMeln <97872920+NastyaMeln@users.noreply.github.com>
2022-04-27 23:55:43 +0700

    Update MvcConfig.java

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    // todo без этих контроллеров отваливается регистрация. За что они отвечают? и где подключаются в конфиге?
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 //        registry.addViewController("/user").setViewName("user");
